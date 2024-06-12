4afbbd80d22dc5406fcd87c16a3f4719b1cfe93f
david-ae <davidedet47@gmail.com>
2024-06-12 22:16:32 +0100

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'wellmanager' title`, () => {
+  it(`should have the 'financecalculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wellmanager');
+    expect(app.title).toEqual('financecalculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wellmanager');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, financecalculator'
+    );
   });
 });
